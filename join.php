16012d8aa6c3d0157dbecb5584214d34f4aa4115
chanki <kgichan12@gmail.com>
2018-04-10 22:12:17 +0900

    조금조금씩 수정한 코드 백업을 위해서 commit 회원가입란에 이름 추가

--- join.php
+++ join.php
@@ -1,4 +1,6 @@
 <?php
+header("Content-Type:text/html;charset=utf-8");
+
  $host = '127.0.0.1';
  $user = 'chanki0508';
  $pw = '';
@@ -7,16 +9,17 @@
  $mysqli = new mysqli($host, $user, $pw, $dbName,$port);
  $email=$_POST['email'];
  $password=md5($_POST['password']);
- $memberId=1;
+ $userName=$_POST['userName'];
+
+
  
- $sql = "insert into usertable (email, password,memberId)";
- echo $sql;
- echo $email;
- echo $password;
- $sql = $sql. "values('$email','$password','$memberId')";
+ $sql = "insert into userInfo (email, password ,name)";
+ $sql = $sql. "values('$email','$password','$userName')";
  if($mysqli->query($sql)){
-  echo 'success inserting';
- }else{
-  echo 'fail to insert sql';
+  echo '회원가입에 성공하셨습니다. 로그인 페이지로 이동합니다.';
+  echo("<script>location.replace('https://database-chanki0508.c9users.io');</script>"); 
+ }
+ else{
+  echo '에러가 발생했습니다. 다시 한번 확인후 가입해주세요.';
  }
 ?>
